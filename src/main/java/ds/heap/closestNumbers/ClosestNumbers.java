c0f43d6d413d188e7996b7be0659f56a753e76b0
Abhishek-9972 <abhisheksecondary1993@gmail.com>
2024-03-18 18:46:48 +0530

    Stack, Queue & Heap

---
+++ src/main/java/ds/heap/closestNumbers/ClosestNumbers.java
@@ -0,0 +1,32 @@
+package ds.heap.closestNumbers;
+
+import ds.heap.sortNearbySortedArray.Pair;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class ClosestNumbers {
+    public static void main(String[] args) {
+        int[] arr = {5, 6, 7, 8, 9};
+        int k = 3;
+        int x = 7;
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>(new Comparator<Pair>() {
+            @Override
+            public int compare(Pair pair1, Pair pair2) {
+                return pair2.getValue().compareTo(pair1.getValue());
+            }
+        });
+
+        for (int i = 0; i < arr.length; i++) {
+            pq.add(new Pair(arr[i], Math.abs(arr[i] - x)));
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        while (pq.size() > 0) {
+            System.out.println(pq.poll().getKey());
+        }
+    }
+}
---
+++ src/main/java/ds/heap/sortNearbySortedArray/SortNearBySortedArray.java
@@ -0,0 +1,47 @@
+package ds.heap.sortNearbySortedArray;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+public class SortNearBySortedArray {
+    public static void main(String[] args) {
+        int[] arr = {6,5,3,2,8,10,9};
+        int k = 3;
+
+        SortNearBySortedArray.kSort(arr, arr.length, k);
+    }
+
+    private static void kSort(int[] arr, int n, int k)
+    {
+
+        // min heap
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        // add first k + 1 items to the min heap
+        for(int i = 0; i < k + 1; i++)
+        {
+            priorityQueue.add(arr[i]);
+        }
+
+        int index = 0;
+        for(int i = k + 1; i < n; i++)
+        {
+            arr[index++] = priorityQueue.poll();
+            priorityQueue.add(arr[i]);
+        }
+
+        Iterator<Integer> itr = priorityQueue.iterator();
+
+        while(itr.hasNext())
+        {
+            arr[index++] = priorityQueue.peek();
+            priorityQueue.poll();
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+}
