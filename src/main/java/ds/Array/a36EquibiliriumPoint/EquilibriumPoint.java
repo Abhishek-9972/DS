0ac2b8bb84048a44a123511a05f84b0d1655bf6f
Abhishek-9972 <abhisheksecondary1993@gmail.com>
2024-05-23 10:14:47 +0530

    Array

---
+++ src/main/java/ds/Array/a36EquibiliriumPoint/EquilibriumPoint.java
@@ -0,0 +1,26 @@
+package ds.Array.a36EquibiliriumPoint;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EquilibriumPoint {
+
+    public int pivotIndex(List<Integer> nums) {
+        int left = 0, pivot = 0, right = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            right += nums.get(i);
+        }
+        while (pivot < nums.size() - 1 && right != left) {
+            pivot++;
+            right -= nums.get(pivot);
+            left += nums.get(pivot - 1);
+        }
+        return (left == right) ? pivot : -1;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1, 7, 3, 6, 5, 6);
+        int result = new EquilibriumPoint().pivotIndex(nums);
+        System.out.println(result);
+    }
+}
