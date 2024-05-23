5a17617d0d7561e41ae3e121fc424995c8538c26
Abhishek-9972 <abhisheksecondary1993@gmail.com>
2024-05-23 22:13:27 +0530

    Array

---
+++ src/main/java/ds/String/a08TaskScheduler/TaskScheduler.java
@@ -0,0 +1,28 @@
+package ds.String.a08TaskScheduler;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/task-scheduler/description/
+ * https://www.youtube.com/watch?v=eGf-26OTI-A
+ */
+public class TaskScheduler {
+    public int leastInterval(char[] tasks, int n) {
+        int[] charMap = new int[26];
+        for (char c : tasks) {
+            charMap[c - 'A']++;
+        }
+
+        Arrays.sort(charMap);
+
+        int maxVal = charMap[25] - 1;
+        int idleSlots = maxVal * n;
+
+        for (int i = 24; i >= 0; i--) {
+            idleSlots = idleSlots - Math.min(charMap[i], maxVal);
+
+        }
+
+        return idleSlots > 0 ? idleSlots + tasks.length : tasks.length;
+    }
+}
