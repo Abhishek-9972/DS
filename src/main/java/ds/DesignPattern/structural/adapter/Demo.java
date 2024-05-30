eac55849e7c764084038b329bc49199a549509fe
Abhishek-9972 <abhisheksecondary1993@gmail.com>
2024-05-30 21:07:06 +0530

    Decorator Design Pattern

---
+++ src/main/java/ds/DesignPattern/structural/adapter/Demo.java
@@ -0,0 +1,11 @@
+package ds.DesignPattern.structural.adapter;
+
+import java.util.List;
+
+public class Demo {
+    public static void main(String[] args) {
+        StudentClient studentClient = new StudentClient();
+        List<Student> studentList = studentClient.getStudentList();
+        System.out.println(studentList);
+    }
+}
---
+++ src/main/java/ds/DesignPattern/structural/adapter/SchoolStudentAdapter.java
@@ -0,0 +1,26 @@
+package ds.DesignPattern.structural.adapter;
+
+public class SchoolStudentAdapter implements Student {
+
+    private SchoolStudent schoolStudent;
+
+    public SchoolStudentAdapter(SchoolStudent schoolStudent){
+        this.schoolStudent = schoolStudent;
+    }
+
+
+    @Override
+    public String getName() {
+        return this.schoolStudent.getName();
+    }
+
+    @Override
+    public String getSurname() {
+        return this.schoolStudent.getSurName();
+    }
+
+    @Override
+    public String getEmail() {
+        return this.schoolStudent.getEmail();
+    }
+}
