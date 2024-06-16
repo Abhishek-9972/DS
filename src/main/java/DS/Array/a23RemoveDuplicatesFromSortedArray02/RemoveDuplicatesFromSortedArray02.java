7696074c27da5e4a504cd03a1fc0525ff717c98f
Abhishek-9972 <abhisheksecondary1993@gmail.com>
2024-06-16 19:23:04 +0530

    Theory

--- src/main/java/DS/Array/a23RemoveDuplicatesFromSortedArray02/RemoveDuplicatesFromSortedArray02.java
+++ src/main/java/DS/Array/a23RemoveDuplicatesFromSortedArray02/RemoveDuplicatesFromSortedArray02.java
@@ -1,7 +1,25 @@
 package DS.Array.a23RemoveDuplicatesFromSortedArray02;
 
 public class RemoveDuplicatesFromSortedArray02 {
+
     public int removeDuplicates(int[] nums) {
+        int counter = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int start = i;
+            while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
+                i++;
+            }
+            if (start != i) {
+                nums[counter++] = nums[i];
+                nums[counter++] = nums[i];
+            } else {
+                nums[counter++] = nums[i];
+            }
+        }
+        return counter;
+    }
+
+    public int removeDuplicates2(int[] nums) {
         int counter = 0;
         for (int i = 0; i < nums.length; i++) {
             while (i + 2 < nums.length && nums[i] == nums[i + 2]) {
