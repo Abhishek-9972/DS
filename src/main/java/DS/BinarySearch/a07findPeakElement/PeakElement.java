d9c54d4097944d4863abca06c8b13e00136a7572
Abhishek-9972 <abhisheksecondary1993@gmail.com>
2024-06-17 17:52:57 +0530

    Theory

--- src/main/java/DS/BinarySearch/a07findPeakElement/PeakElement.java
+++ src/main/java/DS/BinarySearch/a07findPeakElement/PeakElement.java
@@ -2,28 +2,25 @@ package DS.BinarySearch.a07findPeakElement;
 
 public class PeakElement {
 
-    public int binarySearch(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-        int n = arr.length - 1;
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return 0;
+        if (nums[0] > nums[1]) return 0;
+        if (nums[n - 1] > nums[n - 2]) return n - 1;
+        int start = 1, end = n - 2, mid = 0;
         while (start <= end) {
-            int mid = (start + end) / 2;
-            if (arr[mid] >= arr[(mid + 1) % n] && arr[mid] >= arr[(mid + n - 1) % n]) {
-                return mid;
-            }
-            if (arr[start] < arr[mid]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
+            mid = (start + end) / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid;
+            if (nums[mid] > nums[mid + 1]) end = mid - 1;
+            else start = mid + 1;
         }
-        return 0;
+        return -1;
     }
 
     public static void main(String[] args) {
-        int [] arr = {5, 10, 20, 15};
+        int[] arr = {5, 10, 20, 15};
         PeakElement peakElement = new PeakElement();
-        int i = peakElement.binarySearch(arr);
+        int i = peakElement.findPeakElement(arr);
         System.out.println(arr[i]);
     }
 
